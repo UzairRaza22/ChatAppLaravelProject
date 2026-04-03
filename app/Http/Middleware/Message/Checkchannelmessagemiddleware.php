651e23b0f7e78778d6016b90b5fd73918d569701
UzairRaza22 <uzairraza188@gmail.com>
2026-04-03 17:32:36 +0500

    Remove debug logging and fix creator check for ObjectId handling

--- app/Http/Middleware/Message/Checkchannelmessagemiddleware.php
+++ app/Http/Middleware/Message/Checkchannelmessagemiddleware.php
@@ -41,15 +41,6 @@ class CheckChannelMessageMiddleware
         $isCreator = (string) $channel->created_id === $userId || 
                     (is_object($channel->created_id) && (string) $channel->created_id === $userId);
         
-        // Debug logging
-        \Log::info('=== MESSAGE MIDDLEWARE DEBUG ===');
-        \Log::info('Channel: ' . $channel->name);
-        \Log::info('User ID: ' . $userId);
-        \Log::info('Channel created_id: ' . $channel->created_id);
-        \Log::info('Channel created_id type: ' . gettype($channel->created_id));
-        \Log::info('Is creator: ' . ($isCreator ? 'YES' : 'NO'));
-        \Log::info('Raw members: ' . json_encode($channel->members));
-        
         $senderIsMember = $isCreator;
         
         // If not creator, check if user is member
